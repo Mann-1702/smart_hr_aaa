5b6d4bcd67716f7434d3b401c1fb11855be7f3f0
Mann Shah <mannshah1702@gmail.com>
2024-01-17 16:36:36 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/smart_hr_aaa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smart_hr_aaa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:smart_hr_aaa/loginscreen.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+void main() {
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const KeyboardVisibilityProvider(
+        child: LoginScreen()
+      ),
+    );
+  }
+}
+
